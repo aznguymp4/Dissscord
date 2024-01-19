018a72f263404e0a750b01616280a38085bba78e
Erich <erichnguyen25@gmail.com>
2024-01-19 12:43:36 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
 
-app = Flask(__name__, static_folder='../react-vite/dist', static_url_path='/')
+app = Flask(__name__, static_folder='../react-app/dist', static_url_path='/')
 
 # Setup login manager
 login = LoginManager(app)
