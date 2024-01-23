5f0b5359167877657750ba2c95a1c1a814811fe0
Andrew <andrew.madrigal23@gmail.com>
2024-01-23 11:03:13 -0800

    refactored for forms

--- app/api/messages_routes.py
+++ app/api/messages_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, redirect
-from app.models import Channel, db, Message, Server
+from app.models import db, Message
+from app.forms import MessageForm
 from flask_login import current_user,login_required
 
 message_routes = Blueprint("messages", __name__)
@@ -19,15 +20,18 @@ def channel_message(message_id):
 @message_routes.route('<int:message_id>', methods=["PUT"])
 @login_required
 def update_message(message_id):
-  data = request.json
+  form = MessageForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
   message = Message.query.get(message_id)
   if message.author_id == current_user.id:
-    if message:
-      message.content = data["content"]
-      db.session.commit()
-      return message.to_dict()
-    else:
-      return {'error': {'message': "Message could not be found"}}, 404
+    if form.validate_on_submit():
+      if message:
+        message.content = form.data["content"]
+        db.session.commit()
+        return message.to_dict()
+      else:
+        return {'error': {'message': "Message could not be found"}}, 404
+    return form.errors, 401
   return redirect('api/auth/unauthorized')
 
 #delete Message
--- app/forms/server_form.py
+++ app/forms/server_form.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, URLField, BooleanField
+from wtforms.validators import DataRequired, Length
 
 class ServerForm(FlaskForm):
-    displayname = StringField('Server Name', validators=[DataRequired()])
+    displayname = StringField('Server Name', validators=[DataRequired(), Length(max=40, message="Length of server name must be less than 40 characters")])
+    icon = URLField("icon", validators=[Length(max=256, message="Length of URL must be less than 256 characters")])
+    desc = StringField('description', validators=[Length(max=512, message="Length of description must be less than 256 characters")])
+    banner = URLField('banner', validators=[Length(max=256, message="Length of URL must be less than 256 characters")])
+    public = BooleanField('public')
