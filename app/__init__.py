7576a8b222a75d306c955519509238262f93a1ef
Erich <erichnguyen25@gmail.com>
2024-01-26 18:46:50 -0600

    image uploading

--- app/__init__.py
+++ app/__init__.py
@@ -96,44 +96,16 @@ def react_root(path):
         return app.send_from_directory('public', 'favicon.ico')
     return app.send_static_file('index.html')
 
-@app.route('/api/cdn/upload', methods=['POST'])
-def upload_file():
-    f = request.files['files']
-    res = requests.post(
-        os
-        .
-        environ
-        .
-        get
-            (
-            'DISCORD_WEBHOOK'
-            )
-            ,
-            headers
-            =
-            {
-
-            }
-            ,
-            files
-            =
-            [
-        (
-            'file'
-            ,
-            (
-                f
-                .
-                filename
-                ,
-                f
-                )
-                )
-    ]
-    )
-    if res.ok:
-        return res.json()['attachments'], res.status_code
-    return res.json(), 413
+# @app.route('/api/cdn/upload', methods=['POST'])
+# def upload_file():
+#     f = request.files['files']
+#     res = requests.post(os.environ.get('DISCORD_WEBHOOK'), headers={}, files=[
+#         ('file',(f.filename,f))
+#         # ('file',('file.png',f))
+#     ])
+#     if res.ok:
+#         return res.json()['attachments'], res.status_code
+#     return res.json(), 413
 
 @app.errorhandler(404)
 def not_found(e):
