a03be616da85b34725691b974841948e17bc579a
Wilmer <wilmer.sampedro@gmail.com>
2024-02-27 15:34:05 -0800

    reverted css changes, added aws helper file, changed forms for files

--- app/forms/server_form.py
+++ app/forms/server_form.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, URLField, BooleanField
 from wtforms.validators import DataRequired, Length
+from app.api.aws import ALLOWED_EXTENSIONS
 
 class ServerForm(FlaskForm):
     displayname = StringField('Server Name', validators=[DataRequired(), Length(max=128, message="Length of server name must be less than 128 characters")])
-    icon = URLField("icon", validators=[Length(max=256, message="Length of URL must be less than 256 characters")])
+    # icon = URLField("icon", validators=[Length(max=256, message="Length of URL must be less than 256 characters")])
+    icon = FileField("Icon Image File", validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
     desc = StringField('description', validators=[Length(max=512, message="Length of description must be less than 512 characters")])
-    banner = URLField('banner', validators=[Length(max=256, message="Length of URL must be less than 256 characters")])
+    # banner = URLField('banner', validators=[Length(max=256, message="Length of URL must be less than 256 characters")])
+    banner = FileField("Banner Image File", validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
     public = BooleanField('public')
--- app/forms/user_edit_form.py
+++ app/forms/user_edit_form.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
+from app.api.aws import ALLOWED_EXTENSIONS
 
 def username_exists(form, field):
     # Checking if username is already in use
@@ -28,4 +30,5 @@ class UserEditForm(FlaskForm):
     username = StringField('username', validators=[username_exists, Length(max=40, message="Username too long")])
     email = StringField('email', validators=[user_exists])
     bio = StringField('bio', validators=[Length(max=256, message="Bio too long")])
-    icon = StringField('icon')
+    # icon = StringField('icon')
+    icon = FileField("User Image File", validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
