97f4e39dd05055094ff8e29300dd278c6961a1e2
Erich <48527495+aznguymp4@users.noreply.github.com>
2026-03-26 16:44:06 -0500

    Update cdn_routes.py

--- app/api/cdn_routes.py
+++ app/api/cdn_routes.py
@@ -13,7 +13,7 @@ s3 = boto3.client(
   "s3",
   aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
   aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
-  region_name='us-east-2',
+  region_name=os.environ.get('AWS_S3_BUCKET_REGION'),
   config=Config(signature_version='s3v4')
 )
 
